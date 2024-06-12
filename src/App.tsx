e2c43836ddff4bcdb413e8c719228882c8e06155
xArisen <bartoszczapiewski@MacBook-Pro-Bartosz.local>
2024-06-12 17:16:20 +0200

    fix metro builder and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@
  * @format
  */
 
+import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import React from 'react'
 import 'react-native-gesture-handler'
@@ -14,9 +15,11 @@ const Stack = createStackNavigator()
 
 const MyStack = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
-    </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
